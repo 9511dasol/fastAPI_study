c1c6256ba84af75b0f2a28d76d5588e16c384eac
DaSolHan <9511dasole@gmail.com>
2026-02-19 09:22:33 +0900

    update my fastAPI

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from mysite4.models.post import Post
+from database import engine, Base
 from mysite.post_api import router as post_api
 from mysite3.routers.post_router import router as mvc_router
+from mysite4.routers.post_router import router as db_router
+
+# 기존 테이블 지우기
+Base.metadata.drop_all(bind=engine)
+
+# 정의된 모델들을 기반으로 DB에 테이블을 생성한다.
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-routers = [post_api, mvc_router]
+routers = [post_api, mvc_router, db_router]
 
 for router in routers:
     app.include_router(router)
